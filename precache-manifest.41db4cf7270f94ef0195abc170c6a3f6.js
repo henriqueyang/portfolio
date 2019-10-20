1b62362cde9a0047847911e23e8c248fae49e5ec
Henrique <henriqueyang17@gmail.com>
2019-10-20 12:18:08 -0300

    Updates

---
+++ precache-manifest.41db4cf7270f94ef0195abc170c6a3f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52bc6158f168c187aa6d6f42bfe1588",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ecf47d9da34958e5f392",
+    "url": "/portfolio/static/css/main.834715e0.chunk.css"
+  },
+  {
+    "revision": "085d3d45c40c1c90d630",
+    "url": "/portfolio/static/js/2.6b55ec74.chunk.js"
+  },
+  {
+    "revision": "ecf47d9da34958e5f392",
+    "url": "/portfolio/static/js/main.93d35aea.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "f958b6db2b14488108da01dfa97da738",
+    "url": "/portfolio/static/media/eu.f958b6db.png"
+  },
+  {
+    "revision": "60efe521e3efef9168a30787e3384c22",
+    "url": "/portfolio/static/media/mean-logo.60efe521.png"
+  },
+  {
+    "revision": "073ea8b86447ed9310e7e73bd89894b3",
+    "url": "/portfolio/static/media/mern-logo.073ea8b8.png"
+  },
+  {
+    "revision": "7bb2500a0160255607107dedddd8da1c",
+    "url": "/portfolio/static/media/php-mysql-logo.7bb2500a.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/react-logo.47ce6e77.png"
+  },
+  {
+    "revision": "79dfc3a518c6c785008bc7df124700b2",
+    "url": "/portfolio/static/media/tdd.79dfc3a5.png"
+  }
+]);
